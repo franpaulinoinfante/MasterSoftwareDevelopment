9931e0ae51c338f02298497c54ad30fcbb149714
Francisco Javier Paulino Infante <franpaulinoinfan@gmail.com>
2022-10-27 10:27:57 -0400

    MVP-pm Update

--- src/Mastermind/DocumentView/WithFactoryMethod/Mastermind.GameViews/WithGameView.cs
+++ src/Mastermind/DocumentView/WithFactoryMethod/Mastermind.GameViews/WithGameView.cs
@@ -1,19 +1,13 @@
 ﻿using Mastermind.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Mastermind.GameViews
+namespace Mastermind.GameViews;
+
+public abstract class WithGameView
 {
-    public abstract class WithGameView
-    {
-        protected internal Game _game;
+    protected internal Game _game;
 
-        protected WithGameView(Game game)
-        {
-            _game = game;
-        }
+    protected WithGameView(Game game)
+    {
+        _game = game;
     }
 }
